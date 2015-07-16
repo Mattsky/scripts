4182bd2b786a0262cee4048dbca0c2774cb78470
Matt North <matt@cryptechconsulting.co.uk>
2015-07-16 12:35:23 +0100

    IP check script
    Quick and dirty script to pull system names from a file and grab all
    interface IPs for reference. NOTE: can pull multiple addresses but
    output needs fixing (add a comma before info, quick fix to do later)

---
+++ scripts/ip_check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Script to poll tons of systems for IP addresses and drop them
+# into a .csv file for reference.
+# Note: depends on keybased SSH.
+
+# Command to list systems in target dir is:
+# find ./* -type f -exec cat {} \; | grep -v "^\[" | awk '{print $1}' | grep -v "^impact" | grep -v "^$" | grep -v "^#" | grep -v "^node" | grep -v "^security" | grep -v "^configuration" | grep -v "^puppet" | grep -v "This"
+
+# enable cancellation of individual systems if they don't connect properly
+
+trap ctrl_c INT
+
+function ctrl_c() {
+	echo "** Trapped CTRL-C"
+}
+
+echo "Hostname , IP address" > ~/address_list.txt
+
+while read -r line || [[ -n $line ]]; do
+	echo "Connecting to $line"
+	# -n flag makes sure stdin doesn't get eaten and break the loop!
+	RESULTS=$(ssh -n $line "/sbin/ifconfig | grep 'inet addr' | grep -v '127.0.0.1' | awk '{print \$2}' | sed 's/addr\://'")
+	echo "Exit code: $?"
+	if [ $? -eq 0 ]; then
+		echo "$line , $RESULTS" >> ~/address_list.txt	
+	fi
+done < "$1"
