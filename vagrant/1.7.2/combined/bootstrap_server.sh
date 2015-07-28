95c7346053f4ba8f4049fbc0b2709659c83389d4
Matt North <matt@cryptechconsulting.co.uk>
2015-07-28 07:35:09 +0100

    File path fix
    Making sure sed is carried out on the puppet.conf file.

--- vagrant/1.7.2/combined/bootstrap_server.sh
+++ vagrant/1.7.2/combined/bootstrap_server.sh
@@ -3,7 +3,7 @@
         rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
         yum clean all
         yum -y install puppet-server sleep
-        sed -i -e '/vardir\/ssl/a dns_alt_names = puppet,puppet-server,puppet-server.localdomain' /etc/puppet.conf
+        sed -i -e '/vardir\/ssl/a dns_alt_names = puppet,puppet-server,puppet-server.localdomain' /etc/puppet/puppet.conf
         puppet master --verbose --no-daemonize &
         sleep 30
         sed -i -e 's/=permissive/=disabled/' /etc/selinux/config
